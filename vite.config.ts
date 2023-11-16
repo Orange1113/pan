cea7e0f5beacab2fc156fc60934442df05a0d95c
Orange1113 <139049822+Orange1113@users.noreply.github.com>
2023-11-16 20:31:17 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,57 @@
+import path from "path"
+import { defineConfig } from "vite"
+import solidPlugin from "vite-plugin-solid"
+import legacy from "@vitejs/plugin-legacy"
+import { dynamicBase } from "vite-plugin-dynamic-base"
+import copyPlugin from "rollup-plugin-copy"
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "src"),
+      "@solidjs/router": path.resolve(__dirname, "solid-router/src"),
+    },
+  },
+  plugins: [
+    solidPlugin(),
+    legacy({
+      targets: ["defaults"],
+    }),
+    copyPlugin({
+      targets: [
+        { src: "node_modules/libass-wasm/**/*.wasm", dest: "dist/assets" },
+      ],
+      hook: "writeBundle",
+    }),
+    dynamicBase({
+      // dynamic public path var string, default window.__dynamic_base__
+      publicPath: " window.__dynamic_base__",
+      // dynamic load resources on index.html, default false. maybe change default true
+      transformIndexHtml: true,
+    }),
+  ],
+  base: process.env.NODE_ENV === "production" ? "/__dynamic_base__/" : "/",
+  // base: "/",
+  build: {
+    // target: "es2015", //next
+    // polyfillDynamicImport: false,
+  },
+  // experimental: {
+  //   renderBuiltUrl: (filename, { type, hostId, hostType }) => {
+  //     if (type === "asset") {
+  //       return { runtime: `window.ALIST.cdn/${filename}` };
+  //     }
+  //     return { relative: true };
+  //   },
+  // },
+  server: {
+    host: "0.0.0.0",
+    proxy: {
+      "/api": {
+        target: "http://localhost:5244",
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+})
